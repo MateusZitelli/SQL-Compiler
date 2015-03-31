436ead8bfb563ee9897d1925b7b97cd226aed320
jfmorais <jfmoraiscury@gmail.com>
2015-03-31 19:03:32 -0300

    Lex

---
+++ Lex.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+
+public class Lex {
+
+	public static ArrayList<Token> lex (String input){
+		//Os tokens que vão ser retornados
+		ArrayList<Token> tokens = new ArrayList<Token>();
+		
+		return tokens;
+		
+	}
+
+	public static void main(String[] args) {
+		String input = "9213123123 23838 12";
+		ArrayList<Token> tokens = new ArrayList<Token>();
+		tokens = lex(input);
+		
+		
+		for (Token token : tokens){
+			System.out.println(token.toString());
+		}
+		
+		System.out.println("Teste");
+	}
+	
+	
+}
