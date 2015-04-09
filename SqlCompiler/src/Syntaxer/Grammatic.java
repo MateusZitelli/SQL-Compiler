5f5a4bdea4ca1d4c8bf86affbb060055c472e0e6
ArthurMonteforte <arthur.monteforte@hotmail.com>
2015-04-08 21:30:42 -0300

    Update Grammatic.java
    Adicionando os lados esquerdos de cada produção.

--- SqlCompiler/src/Syntaxer/Grammatic.java
+++ SqlCompiler/src/Syntaxer/Grammatic.java
@@ -1,3 +1,7 @@
 public enum Grammatic {
     Elemento, ElementoPrime, Coluna;
+    /*Start, Create, UDatabase, ATable, DTable, CmdInsert, CmdDelete, CmdSelect, Create', CDatabase, CTable, ConteudoTabela, Elemento,
+	Coluna, Elemento', DataType, Condition, Stmt, Columns, ColumnValue, Columns', Columns'', ColumnValue', ColumnValue'',
+	CmdWhere, Whereclausule, Operator, Whereclausule', Logical, Tables, Tables';
+	*/
 }
