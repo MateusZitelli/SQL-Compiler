e442888e1d28452a3d41ca5004029bd3766ac08f
Mateus Zitelli <zitellimateus@gmail.com>
2015-04-09 11:28:35 -0300

    Add syntax analyzer

--- SqlCompiler/src/Syntaxer/Syntaxer.java
+++ SqlCompiler/src/Syntaxer/Syntaxer.java
@@ -1,8 +1,55 @@
+import java.util.Stack;
+import java.util.ArrayList;
 import Tokenizer.*;
 
 public class Syntaxer { 
-    public static void test() {
-        ParsingTable t = new ParsingTable();
-        System.out.println(t.getRule(TokenType.COMMA, Grammatic.ElementoPrime));
+    public static ParsingTable table;
+    public static Stack stack;
+
+    public static void analyse(ArrayList<Token> tokensList) {
+        int position = 0;
+        Token token;
+        Production rule;
+        Object expectedInput;
+
+        while(stack.size() > 0) {
+            token = tokensList.get(position); 
+            expectedInput = stack.pop(); 
+            if(expectedInput instanceof TokenType){
+                if(expectedInput == token.type){
+                    position += 1; 
+                    System.out.print("pop ");
+                    System.out.println(token);
+                    if(expectedInput == TokenType.EOF){
+                        System.out.println("Accepted");
+                    }
+                }else{
+                    System.out.print("Bad input: ");
+                    System.out.println(token);
+                    return;
+                }
+            }else if(expectedInput instanceof Grammatic){
+                rule = table.getRule(token.type, (Grammatic)expectedInput); 
+                if(rule == null){
+                    System.out.print("Bad input: ");
+                    System.out.println(token);
+                    return;
+                }
+                System.out.print("Rule: ");
+                System.out.println(rule);
+                // Stack expected instructions
+                for(int i = rule.productions.length - 1; i >= 0; i--){
+                    stack.push(rule.productions[i]);
+                }
+            }
+            System.out.print("Stack: ");
+            System.out.println(stack);
+        }
+    }
+
+    public Syntaxer() {
+        this.table = new ParsingTable();
+        stack.push(TokenType.EOF); 
+        stack.push(Grammatic.START);
     }
 }
