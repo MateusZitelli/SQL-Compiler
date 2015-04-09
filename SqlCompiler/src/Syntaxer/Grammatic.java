51c90fc53255158f3908b18ee731ab83ac71c745
ArthurMonteforte <arthur.monteforte@hotmail.com>
2015-04-08 22:16:00 -0300

    Update Grammatic.java

--- SqlCompiler/src/Syntaxer/Grammatic.java
+++ SqlCompiler/src/Syntaxer/Grammatic.java
@@ -1,6 +1,6 @@
 public enum Grammatic {
     Elemento, ElementoPrime, Coluna;
-    /*Start, Create, UDatabase, ATable, DTable, CmdInsert, CmdDelete, CmdSelect, Create', CDatabase, CTable, ConteudoTabela, Elemento,
+    /*START, Create, UDatabase, ATable, DTable, CmdInsert, CmdDelete, CmdSelect, Create', CDatabase, CTable, ConteudoTabela, Elemento,
 	Coluna, Elemento', DataType, Condition, Stmt, Columns, ColumnValue, Columns', Columns'', ColumnValue', ColumnValue'',
 	CmdWhere, Whereclausule, Operator, Whereclausule', Logical, Tables, Tables';
 	*/
