dd8364e8cfdbef54676b1c0111727f4d4a939f69
Mateus Zitelli <zitellimateus@gmail.com>
2015-04-22 16:51:00 -0300

    Fix primary key support

--- SqlCompiler/bin/script.sql
+++ SqlCompiler/bin/script.sql
@@ -1,3 +1,16 @@
-insert into teste (nome, outroNome) values ("teste");
-insert into teste (nome, outroNome) values ("teste");
-insert into teste (nome, outroNome) values ("teste");
+drop table primary_117;
+CREATE TABLE Persons(
+P_Id numeric(1) NOT NULL PRIMARY KEY,
+LastName varchar(255) NOT NULL,
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+)
+
+insert into primary_117 (1,3);
+insert into primary_117 (2,4);
+insert into primary_117 (3,5);
+
+select * from primary_117;
+update primary_117 set c1=7 where c2=4;
+select * from primary_117;
