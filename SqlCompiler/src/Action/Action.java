0f7fc085931dcafab47d2b294be83e5cfc434241
Mateus Zitelli <zitellimateus@gmail.com>
2015-04-23 00:16:56 -0300

    Trying

--- SqlCompiler/src/Action/Action.java
+++ SqlCompiler/src/Action/Action.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class Action implements Attributable { 
     public Map<String, String> attrs = new HashMap<String, String>();
+    public String getAttr(String key) {
+        return attrs.get(key);
+    }
+
+    public void setAttr(String key, String value) {
+        attrs.put(key, value);
+    }
+}
+public class WhileAct extends Action implements Actionable {
+    public void act(ArrayList<Object> stack){
+        stack.get(stack.size() - 3).setAttr("teste", "false");
+    } 
 }
